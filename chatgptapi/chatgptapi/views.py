a2da51a82308f65b671dd010a5526cdaa5a1837c
Kasirajan Alayappan <kasirajan.alayappan@ideas2it.com>
2023-03-06 00:08:59 +0530

    Updated to use openai

--- chatgptapi/chatgptapi/views.py
+++ chatgptapi/chatgptapi/views.py
@@ -1,42 +1,26 @@
+import openai
+import os
+
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import NotAuthenticated
-from pyChatGPT import ChatGPT
 from rest_framework.response import Response
+
+
+# Set up the OpenAI API credentials
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 class ChatGPTAPI(viewsets.GenericViewSet):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.api = None
-
-    @action(detail=False, methods=['POST'], url_path='login')
-    def login(self, request):
-        """
-        # auth with Google login
-        self.api = ChatGPT(auth_type='google', email='example@gmail.com', password='password')
-
-        # auth with microsoft login
-        api = ChatGPT(auth_type='microsoft', email='example@gmail.com', password='password')
-
-        # auth with openai login (manual captcha solving)
-        api = ChatGPT(
-            auth_type='openai', captcha_solver='',
-            email='example@gmail.com', password='password'
-        )
-        """
-        data = request.data
-        self.api = ChatGPT(auth_type=data['auth_type'], email=data['email'], password=data['password'],
-                           captcha_solver='')
-        return Response("Login Successful", status=200)
-
     @action(detail=False, methods=['POST'], url_path='prompt')
     def prompt(self, request):
         data = request.data
-        if not self.api:
-            raise NotAuthenticated()
+        # Generate a response
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=data["prompt"]
+        )
 
-        resp = self.api.send_message(data['prompt'])
-        print(resp['message'])
-        return Response({"response": resp['message']}, status=200)
+        # Print the generated response
+        print(response.choices[0].text)
+        return Response({"response": response.choices[0].text}, status=200)
